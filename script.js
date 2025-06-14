ffc9f716e3f3190470177e27a5d260b77b8502e9
TAMIL SELVAN <tamilnaresh62@gmail.com>
2025-06-14 18:40:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+  window.onscroll = () =>
+            document.querySelector("header").classList.toggle('scrolled', window.scrollY > 0);
+
+   
+  const links = document.querySelectorAll('.nav-link');
+  const sections = document.querySelectorAll('section');
+
+  window.addEventListener('scroll', () => {
+    sections.forEach(sec => {
+      if (window.scrollY >= sec.offsetTop - 70) {
+        links.forEach(link => {
+          link.classList.remove('active');
+          if (link.getAttribute('href') === `#${sec.id}`) {
+            link.classList.add('active');
+          }
+        });
+      }
+    });
+  });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("in-view");
+        } else {
+          entry.target.classList.remove("in-view");
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
+
+  document.querySelectorAll(".slide-left, .slide-right").forEach((el) => {
+    observer.observe(el);
+  });
+});
+
+
+  const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+  scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+  window.addEventListener("scroll", () => {
+    // Show only after scrolling down a bit
+    scrollTopBtn.style.display = window.scrollY > 300 ? "block" : "none";
+  });
+
+  // Initially hide the button
+  scrollTopBtn.style.display = "none";
+
+
